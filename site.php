c4bd9c9ce6d00b0aa2ac3ba26e6ad3675501ef28
UwinezaErnestine <uwinezae20@gmail.com>
2025-04-13 10:06:18 +0200

    Add files via upload

---
+++ site.php
@@ -0,0 +1,17 @@
+<?php
+  // A simple PHP variable
+  $message = "Hello, welcome to my PHP site!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My PHP Site</title>
+</head>
+<body>
+  <h1><?php echo $message; ?></h1>
+  <p>This is a simple example of PHP working inside HTML.</p>
+</body>
+</html>
